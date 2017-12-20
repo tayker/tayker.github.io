d00dd2a6299b12bd6c9e151250458b218caa8672
tayker <ser.mazur501@gmail.com>
2017-12-20 22:54:00 +0200

    'headers'

---
+++ portfolio/projects/headers/dist/js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    alert('sds');
+    $('.header--fifth__slider').slick({
+        arrows: false,
+        autoplay: true,
+        dots: true,
+        dotsClass: "dots-class"
+    });
+});
